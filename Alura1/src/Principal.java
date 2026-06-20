ffb7fb05d65d0b2dd1cf417345bd951db1665e59
MiguelCamMtz <miguelmotzkuscamara@gmail.com>
2026-06-19 23:08:56 -0300

    Java: aplicando a Orientação a Objetos 2-Aproveitando o modelo para series

---
+++ Alura1/src/Principal.java
@@ -0,0 +1,22 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main() {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso Chefao");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliacoes:" + meuFilme.getTotalDeAvaliacoes());
+         System.out.println(meuFilme.pegaMedia());
+        // meuFilme.somaDasAvaliacoes = 10;
+        // meuFilme.totalDeAvaliacoes = 1;
+        // ystem.out.println(meuFilme.pegaMedia());
+
+    }
+}
